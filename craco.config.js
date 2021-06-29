647bd174a835bec9496cbfa4eabffb839e762dcd
tonycashbbb <tonycashbbb@gmail.com>
2021-06-29 14:56:47 +0300

    fix: go-to alias

--- craco.config.js
+++ craco.config.js
@@ -4,7 +4,6 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@hoc': path.resolve(__dirname, 'src/hoc'),
       '@redux': path.resolve(__dirname, 'src/redux'),
