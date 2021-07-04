68985f25dd2790e5bf89ffd6a875dbc3666c315b
tonycashbbb <tonycashbbb@gmail.com>
2021-07-04 18:07:47 +0300

    eslint and prettier config added

--- src/components/Tender/TenderList/TenderList.jsx
+++ src/components/Tender/TenderList/TenderList.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import s from "./TenderList.module.css";
 import { NavLink } from "react-router-dom";
+
 import { Button } from "@components";
 import { APP_TEXT } from "@app/i18n";
+
+import s from "./TenderList.module.css";
 
 const {
   statuses: { FEATURED, ONGOING, CANCELED, ARCHIVED, RETENDER },
@@ -38,66 +40,64 @@ const TenderList = ({
 
         {listItems.length !== 0 ? (
           <ul className={s.list}>
-            {listItems.map((tender) => {
-              return (
-                <li key={tender.id}>
-                  <div className={s.left}>
-                    <div className={s.list__title}>
-                      {tender.contractorId === userId ? (
-                        <NavLink to={`/account/tenders/${tender.id}`}>
-                          {tender.name}
-                        </NavLink>
-                      ) : (
-                        <NavLink to={`/home/tenders/${tender.id}`}>
-                          {tender.name}
-                        </NavLink>
-                      )}
-                    </div>
-                    {isAccount && (
-                      <div className={s.subtitle}>{tender.status}</div>
-                    )}
-                    {!isAccount && (
-                      <div className={s.description}>
-                        <p>{tender.description}</p>
-                      </div>
+            {listItems.map((tender) => (
+              <li key={tender.id}>
+                <div className={s.left}>
+                  <div className={s.list__title}>
+                    {tender.contractorId === userId ? (
+                      <NavLink to={`/account/tenders/${tender.id}`}>
+                        {tender.name}
+                      </NavLink>
+                    ) : (
+                      <NavLink to={`/home/tenders/${tender.id}`}>
+                        {tender.name}
+                      </NavLink>
                     )}
                   </div>
                   {isAccount && (
-                    <div className={s.right}>
-                      <div className={s.startBtn}>
-                        {tender.status === FEATURED && (
-                          <Button
-                            btnColor={"#F4F4F4"}
-                            textColor={"#000"}
-                            onClick={() => startTender(tender.id, userId)}
-                          >
-                            {start}
-                          </Button>
-                        )}
-                        {tender.status === ONGOING && (
-                          <Button
-                            btnColor={"#F4F4F4"}
-                            textColor={"#000"}
-                            onClick={() => cancelTender(tender.id, userId)}
-                          >
-                            {cancel}
-                          </Button>
-                        )}
-                        {statuses.includes(tender.status) && (
-                          <Button
-                            btnColor={"#F4F4F4"}
-                            textColor={"#000"}
-                            onClick={() => retender(tender.id, userId)}
-                          >
-                            {retenderText}
-                          </Button>
-                        )}
-                      </div>
+                    <div className={s.subtitle}>{tender.status}</div>
+                  )}
+                  {!isAccount && (
+                    <div className={s.description}>
+                      <p>{tender.description}</p>
                     </div>
                   )}
-                </li>
-              );
-            })}
+                </div>
+                {isAccount && (
+                  <div className={s.right}>
+                    <div className={s.startBtn}>
+                      {tender.status === FEATURED && (
+                        <Button
+                          btnColor="#F4F4F4"
+                          textColor="#000"
+                          onClick={() => startTender(tender.id, userId)}
+                        >
+                          {start}
+                        </Button>
+                      )}
+                      {tender.status === ONGOING && (
+                        <Button
+                          btnColor="#F4F4F4"
+                          textColor="#000"
+                          onClick={() => cancelTender(tender.id, userId)}
+                        >
+                          {cancel}
+                        </Button>
+                      )}
+                      {statuses.includes(tender.status) && (
+                        <Button
+                          btnColor="#F4F4F4"
+                          textColor="#000"
+                          onClick={() => retender(tender.id, userId)}
+                        >
+                          {retenderText}
+                        </Button>
+                      )}
+                    </div>
+                  </div>
+                )}
+              </li>
+            ))}
           </ul>
         ) : (
           <div className={s.noTenders}>{APP_TEXT.tenderList.noTenders}</div>
